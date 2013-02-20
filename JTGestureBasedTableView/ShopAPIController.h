81079e708d79c6a7998aa2c8f14fb0ccc183dce5
Ben Davey <ben@mobilenationhq.com>
2013-02-19 21:37:29 -0800

    will it blend

---
+++ JTGestureBasedTableView/ShopAPIController.h
@@ -0,0 +1,13 @@
+//
+//  ShopAPIController.h
+//  JTGestureBasedTableViewDemo
+//
+//  Created by Ben Davey on 2/19/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShopAPIController : NSObject
+
+@end
---
+++ JTGestureBasedTableView/ShopAPIController.m
@@ -0,0 +1,13 @@
+//
+//  ShopAPIController.m
+//  JTGestureBasedTableViewDemo
+//
+//  Created by Ben Davey on 2/19/13.
+//
+//
+
+#import "ShopAPIController.h"
+
+@implementation ShopAPIController
+
+@end
